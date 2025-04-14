8992f535fd6c3553d9b22015a246b966f7d891ed
mhbrownie <mike@eportant.com>
2025-04-14 08:18:58 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 
 function extractLabels(obj, result = {}) {
   if (Array.isArray(obj)) {
