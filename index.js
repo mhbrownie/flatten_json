3e6d92c7ff38f5681593838b74b275a059a694d8
mhbrownie <mike@eportant.com>
2025-04-15 05:19:35 -0700

    Update index.js
    1

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: "20mb" }));
+
 
 function simplifyAnswers(answers) {
   const simplified = {};
